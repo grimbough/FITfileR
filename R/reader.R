36a46c6428949d6f305f104bfd80187c122eb7e6
Mike Smith <grimbough@gmail.com>
2017-02-26 00:08:33 +0100

    Improved reading of profile.xlsx for data types

--- R/reader.R
+++ R/reader.R
@@ -8,7 +8,8 @@ readFitFile <- function(fileName, dropUnknown = TRUE) {
   scan_result <- .scanFile(fileName)
   ## create a scaffold to fit the data in
   scaffold <- .buildMessageStructure(scan_result[[1]], scan_result[[2]])
-  all_records <- .readFileWithScaffold(fileName, scaffold = scaffold, message_defs = scan_result[[1]])
+  #all_records <- .readFileWithScaffold(fileName, scaffold = scaffold, message_defs = scan_result[[1]])
+  all_records <- .readFileWithScaffold2(fileName, scaffold = scaffold, message_defs = scan_result[[1]])
   all_records <- .renameMessages(all_records, scan_result[[1]])
   
   for(i in names(all_records)) {
@@ -56,6 +57,53 @@ readFitFile <- function(fileName, dropUnknown = TRUE) {
       message <- .readMessage.data(con, message_defs[[ defIdx ]])
       currentRow <- defs_count[[ defIdx ]]
       scaffold[[ defIdx ]][ currentRow , ] <- message$message
+      defs_count[[ defIdx ]] <- defs_count[[ defIdx ]] + 1
+      
+    } else {
+      stop("unknown message type")
+    }
+  }
+  
+  return(scaffold)
+}
+
+
+.readFileWithScaffold2 <- function(fileName, scaffold, message_defs) {
+  
+  con <- file(fileName, "rb")
+  on.exit(close(con))
+  file_header <- .readFileHeader(con)
+  
+  plmt <- "-1";
+  prev_lmt <- "0"
+  defs_count <- list()
+  pseudoMessageTab <- NULL
+  
+  while(seek(con, where = NA) < (file_header$data_size + 14)) {
+    
+    record_header <- .readRecordHeader(con)
+    lmt <- as.character(record_header$local_message_type)
+    if(record_header$message_type == "definition") {
+      
+      if(lmt == prev_lmt) {
+        plmt <- as.character(as.integer(plmt) + 1)
+      } else {
+        plmt <- lmt
+      }
+      pseudoMessageTab <- rbind(pseudoMessageTab, c(lmt, plmt))
+      prev_lmt <- lmt
+      
+      ## read the message definition just to get through the bytes
+      message <- .readMessage.definition(con, devFields = record_header$developer_data)
+      
+      defs_count[[ plmt ]] <- 1
+      
+    } else if(record_header$message_type == "data") {
+      
+      defIdx <- pseudoMessageTab[ max(which(pseudoMessageTab[,1] == lmt)), 2]
+      currentRow <- defs_count[[ defIdx ]]
+      scaffold <- .readMessage.data2(con, message_defs[[ defIdx ]],
+                                     scaffold, defIdx, currentRow)
       defs_count[[ defIdx ]] <- defs_count[[ defIdx ]] + 1
       
     } else {
