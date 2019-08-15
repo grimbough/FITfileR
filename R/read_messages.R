abd2320a66f05fdf8dec3c1fcd553685c465b6e0
Mike Smith <grimbough@gmail.com>
2019-08-15 09:12:40 +0200

    Reading array fields

--- R/read_messages.R
+++ R/read_messages.R
@@ -1,67 +1,70 @@
 .processFieldDefs <- function(fields) {
-  
-  fields <- as.integer(fields)
-  fields <- split(fields, rep(1:3, by = length(fields)/3))
-  names(fields) = c('field_def_num', 'size', 'base_type')
-  fields <- as_data_frame(fields) %>%
-    mutate(base_type = format(as.hexmode(base_type), width = 2))
-  return(fields)
+    
+    fields <- as.integer(fields)
+    fields <- split(fields, rep(1:3, by = length(fields)/3))
+    names(fields) = c('field_def_num', 'size', 'base_type')
+    fields <- as_data_frame(fields) %>%
+        mutate(base_type = format(as.hexmode(base_type), width = 2))
+    return(fields)
 }
 
 .readMessage.definition <- function(con, devFields = FALSE) {
-  
-  message <- list()
-  message$reserved <- readBin(con = con, what = "raw", n = 1, size = 1)
-  message$architecture <- ifelse(readBin(con = con, what = "int", n = 1, size = 1),
-                                 "big", "little")
-  message$global_message_num <- readBin(con = con, what = "int", n = 1, size = 2,
-                                        endian = message$architecture)
-  message$n_fields <- readBin(con = con, what = "int", n = 1, size = 1)
-  message$field_definition <- .processFieldDefs(
-    readBin(con = con, what = "raw", n = 3 * message$n_fields, size = 1)
-  )
-  if(devFields){
-    ## do something with the developer fields
-    ## currently not supported
-    messages$n_dev_fields <- readBin(con = con, what = "int", n = 1, size = 1)
-    message$dev_field_definition <- .processFieldDefs(
-      readBin(con = con, what = "raw", n = 3 * message$n_dev_fields, size = 1)
+    
+    message <- list()
+    message$reserved <- readBin(con = con, what = "raw", n = 1, size = 1)
+    message$architecture <- ifelse(readBin(con = con, what = "int", n = 1, size = 1),
+                                   "big", "little")
+    message$global_message_num <- readBin(con = con, what = "int", n = 1, size = 2,
+                                          endian = message$architecture)
+    message$n_fields <- readBin(con = con, what = "int", n = 1, size = 1)
+    message( message$n_fields )
+    message$field_definition <- .processFieldDefs(
+        readBin(con = con, what = "raw", n = 3 * message$n_fields, size = 1)
     )
-  }
-  return(list(message = message))
+    if(devFields){
+        ## do something with the developer fields
+        ## currently not supported
+        messages$n_dev_fields <- readBin(con = con, what = "int", n = 1, size = 1)
+        message$dev_field_definition <- .processFieldDefs(
+            readBin(con = con, what = "raw", n = 3 * message$n_dev_fields, size = 1)
+        )
+    }
+    return(list(message = message))
 }
 
 
 .readMessage.data <- function(con, definition, compressed_timestamp = FALSE) {
-  
-  fieldTypes <- definition$field_definition$base_type
-  
-  message <- list()
-  for(i in seq_along(fieldTypes)) {
-    readInfo <- data_type_lookup[[ fieldTypes[i] ]]
-    ## if we have a compressed timestamp this is now missing
-    #if(compressed_timestamp && definition$field_definition$field_def_num[i] == 253 ) {
-    #  message[[i]] <- 0
-    #} 
-    #else {
-      message[[i]] <- readBin(con, what = readInfo[[1]], signed = readInfo[[2]],
-                              size = readInfo[[3]], n = readInfo[[4]], 
-                              endian = definition$architecture)
-
-      ## if we have unsigned ints, turn the bits into a numeric
-      if(fieldTypes[i] %in% c('86', '8c')) {
-        if(definition$architecture == "little") {
-          bits <- as.logical(rawToBits(message[[i]][1:4]))
-        } else {
-          bits <- as.logical(rawToBits(message[[i]][4:1]))
+    
+    fieldTypes <- definition$field_definition$base_type
+    sizes <- definition$field_definition$size
+    
+    message <- list()
+    for(i in seq_along(fieldTypes)) {
+        readInfo <- data_type_lookup[[ fieldTypes[i] ]]
+        
+        if(is.null(readInfo)) { stop('Something when wrong. Unknown field type') }
+        
+        ## a single field can have an array of values 
+        single_size <- prod(as.integer(readInfo[3:4]))
+        for(j in seq_len( sizes[i] %/% single_size ) ) {
+            message[[i]] <- readBin(con, what = readInfo[[1]], signed = readInfo[[2]],
+                                    size = readInfo[[3]], n = readInfo[[4]], 
+                                    endian = definition$architecture)
+            
+            ## if we have unsigned ints, turn the bits into a numeric
+            if(fieldTypes[i] %in% c('86', '8c')) {
+                if(definition$architecture == "little") {
+                    bits <- as.logical(rawToBits(message[[i]][1:4]))
+                } else {
+                    bits <- as.logical(rawToBits(message[[i]][4:1]))
+                }
+                message[[i]] <- sum(2^(.subset(0:31, bits)))
+            }
         }
-        message[[i]] <- sum(2^(.subset(0:31, bits)))
-      }
-  #  }
-  }
-
-  message <- data.frame(message)
-  colnames(message) <- definition$field_definition$field_def_num
-  return(list(message = message))
+    }
+    
+    message <- data.frame(message)
+    colnames(message) <- definition$field_definition$field_def_num
+    return(list(message = message))
 }
 
