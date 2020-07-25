cb4359cc59cec6997fccccbfa5a725948f37771e
Mike Smith <grimbough@gmail.com>
2020-07-25 18:16:20 +0200

    Added some documentation

--- R/reader.R
+++ R/reader.R
@@ -1,4 +1,31 @@
-  
+#' Read a FIT file
+#' 
+#' Reads a given FIT file and returns a list of data.frames, one for each
+#' message type stored in the input file.
+#' 
+#' @param fileName A character specifying the FIT file to be read.
+#' @param dropUknown Many FIT files contain data that is not defined in the FIT
+#' file specification.  This may be used by the device manufacturer for 
+#' debugging purposes, but is typically not useful to an end user.  The default
+#' value of this argument will exclude these fields from the returned data 
+#' structure.  Setting a value of \code{FALSE} will retain them.
+#' @param mergeMessages FIT files may contain similar 'messages' with varying 
+#' numbers of fields e.g. if a new sensor is added during an activity the 
+#' 'records' messages recorded after this will contain an extra data column.
+#' The default value of this argument will merge all messages of the same type,
+#' and insert \code{NA} to pad missing fields.  Setting this to \code{FALSE}
+#' will return a separate \code{data.frame} for each distinct message type.
+#' @return A \code{list} of \code{tibbles}.  Each \code{tibble} holds a 
+#' message type defined in the input file.  The list entries are named 
+#' according to the message type.
+#' 
+#' @examples
+#' garmin_file <- system.file("extdata/Garmin.fit", package = "fitFileR")
+#' garmin <- readFitFile(garmin_file)
+#' 
+#' ## examine the 'file ID' messages
+#' garmin$file_id
+#'       
 #' @export
 readFitFile <- function(fileName, dropUnknown = TRUE, mergeMessages = TRUE) {
   
