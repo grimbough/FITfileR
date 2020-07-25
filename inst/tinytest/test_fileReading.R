8090aad3e7ef36e598d21c79061c5b62fca79991
Mike Smith <grimbough@gmail.com>
2020-07-25 17:45:47 +0200

    Add initial package tests

---
+++ inst/tinytest/test_fileReading.R
@@ -0,0 +1,16 @@
+
+garmin_file <- system.file("extdata/Garmin.fit", package = "fitFileR")
+tomtom_file <- system.file("extdata/TomTom.fit", package = "fitFileR")
+
+## test we can read our two example files
+expect_silent(
+    garmin <- readFitFile(garmin_file)
+)
+
+expect_silent(
+    tomtom <- readFitFile(tomtom_file)
+)
+
+## check we got a list back
+expect_true( class(garmin) == "list" )
+expect_true( class(tomtom) == "list" )
