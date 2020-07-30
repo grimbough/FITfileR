485b9eb7ca8696bcc0f5c271b3d21c0b8eabcdd2
Mike Smith <grimbough@gmail.com>
2020-07-30 17:48:11 +0200

    Add some utility functions

--- R/utilities.R
+++ R/utilities.R
@@ -17,3 +17,23 @@
     }
     return(res)
 }
+
+## create a signature for each definition message
+## so we can easily separate message of the same type 
+.definitionSignature <- function(message) {
+  paste(message@definition@field_defs$field_def_num, 
+        message@definition@field_defs$size, 
+        sep = "x",
+        collapse = "_")
+}
+
+## for a given local message type, find the most recent definition message
+## using that local message type.
+.matchDefinition <- function(msgDefs, local_message_number) {
+  for(i in rev(seq_along(msgDefs))) {
+    def <- msgDefs[[i]]
+    if(def@header@local_message_number == local_message_number) {
+      return(def)
+    }
+  }
+}
