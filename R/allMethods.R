839a352ecf24118e99e08b93a4648847a5731423
Mike Smith <grimbough@gmail.com>
2020-07-30 17:02:09 +0200

    Radical overhaul of classes and methods

--- R/allMethods.R
+++ R/allMethods.R
@@ -1,17 +1,147 @@
 #' @export
-setGeneric("records", function(object) {
-    standardGeneric("records")
+setGeneric("getMessagesByType", function(object, global_message_number) {
+    standardGeneric("getMessagesByType")
 })
 
-setMethod("records", signature = "FitFile", function(object) {
-    object@records@messages
-})
+.definitionSignature <- function(message) {
+    paste(message@definition@field_defs$field_def_num, 
+          message@definition@field_defs$size, 
+          sep = "x",
+          collapse = "_")
+}
+
+setMethod("getMessagesByType", 
+          signature = c("RawFitFile", "integer"),
+          function(object, global_message_number) {
+              
+              idx <- vapply(object@messages, FUN = globalMessageNumber, 
+                            FUN.VALUE = integer(1)) == global_message_number
+              
+              if(length(idx)) {
+                  messages <- object@messages[ idx ]
+                  
+                  signatures <- vapply(messages, .definitionSignature, 
+                                       FUN.VALUE = character(1))
+                  
+                  messages2 <- split(messages, signatures)
+                  
+                  messages3 <- lapply(messages2, FUN = function(x) {
+                      message_table <- lapply(x, function(y) {
+                          y@fields 
+                          } 
+                        ) %>% dplyr::bind_rows( ) %>%
+                          mutate(across(everything(), 
+                                        ~ .applyScaleAndOffset(input = ., 
+                                                               as.integer(cur_column()), 
+                                                               global_message_number) 
+                                        ))
+                     names(message_table) <- vapply( as.integer(names(message_table)),
+                                                     FUN = .translateField2, 
+                                                     FUN.VALUE = character(1),
+                                                      global_message_number )
+                      return(message_table)
+                  })
+                  
+                  if(length(messages3) == 1) {
+                      messages3 <- messages3[[1]]
+                  }
+                  return(messages3)
+              } else {
+                  return(NULL)
+              }
+              
+          }
+)
+
 
 #' @export
-setGeneric("laps", function(object) {
-    standardGeneric("laps")
+setGeneric("localMessageNumber", function(object) {
+    standardGeneric("localMessageNumber")
 })
 
-setMethod("laps", signature = "FitFile", function(object) {
-    object@laps@messages
+setMethod("localMessageNumber", 
+          signature = "FitMessageHeader",
+          function(object) {
+              object@local_message_number
+          }
+)
+
+setMethod("localMessageNumber", 
+          signature = "FitDefinitionMessage",
+          function(object) {
+              localMessageNumber(object@header)
+          }
+)
+
+setMethod("localMessageNumber", 
+          signature = "FitDataMessage",
+          function(object) {
+              localMessageNumber(object@definition)
+          }
+)
+
+########################
+## Global Message Number
+########################
+
+#' @export
+setGeneric("globalMessageNumber", function(object) {
+    standardGeneric("globalMessageNumber")
 })
+
+setMethod("globalMessageNumber", 
+          signature = "FitDefinitionMessage",
+          function(object) {
+              object@global_message_number
+          }
+)
+
+setMethod("globalMessageNumber", 
+          signature = "FitDataMessage",
+          function(object) {
+              globalMessageNumber(object@definition)
+          }
+)
+
+## Show
+setMethod("show", 
+          signature = "RawFitFile", 
+          function(object) {
+              cat("Raw Fit File\n")
+              cat("Number of messages: ", length(object@messages), sep = "")
+          }
+)
+
+setMethod("show", signature = "FitDataMessage", function(object) {
+    
+    cat("Local message type: ", localMessageNumber(object),  " ", sep = "")
+    cat("(message name: ", .translateGlobalMessageNumber( globalMessageNumber(object) ), ", ",  sep = "")
+    cat("message type: ", object@definition@global_message_number, ", ", sep = "")
+    cat("fields: ", nrow(object@definition@field_defs), ", bytes: ", sum(object@definition@field_defs$size), ")", sep = "")
+    
+    for(field in object@definition@field_defs$field_def_num) {
+        translated <- .translateField(field, object@definition@global_message_number)
+        cat("\n ", translated$value, " (", translated$key, ", ", translated$type, ")", sep = "")
+        
+        original <- object@fields[[ paste(field) ]]
+        adjusted <- .applyScaleAndOffset( original, field, object@definition@global_message_number )
+        
+        cat(" ", adjusted, " (", original, ")", sep = "")
+        
+    }
+})
+
+## Dump
+
+setGeneric("dump", function(object) {
+    standardGeneric("dump")
+})
+
+setMethod("dump", signature = "RawFitFile",
+          function(object) {
+              object@messages
+          }
+)
+
+
+
--- R/read_messages.R
+++ R/read_messages.R
@@ -8,26 +8,36 @@
     return(fields)
 }
 
-.readMessage.definition <- function(con, devFields = FALSE) {
-    
-    message <- list()
-    message$reserved <- readBin(con = con, what = "raw", n = 1, size = 1)
-    message$architecture <- ifelse(readBin(con = con, what = "int", n = 1, size = 1),
+.readMessage_definition <- function(con, message_header) {
+
+    reserved <- readBin(con = con, what = "raw", n = 1, size = 1)
+    architecture <- ifelse(readBin(con = con, what = "int", n = 1, size = 1),
                                    "big", "little")
-    message$global_message_num <- readBin(con = con, what = "int", n = 1, size = 2,
-                                          endian = message$architecture)
-    message$n_fields <- readBin(con = con, what = "int", n = 1, size = 1)
-    message$field_definition <- .processFieldDefs(
-        readBin(con = con, what = "raw", n = 3 * message$n_fields, size = 1, signed = FALSE)
+    global_message_num <- readBin(con = con, what = "int", n = 1, size = 2,
+                                          endian = architecture)
+    n_fields <- readBin(con = con, what = "int", n = 1, size = 1)
+    field_definition <- .processFieldDefs(
+        readBin(con = con, what = "raw", n = 3 * n_fields, size = 1, signed = FALSE)
     )
-    if(devFields){
+    if(message_header@has_developer_data){
         ## do something with the developer fields
-        message$n_dev_fields <- readBin(con = con, what = "int", n = 1, size = 1, signed = FALSE)
-        message$dev_field_definition <- .processFieldDefs(
-            readBin(con = con, what = "raw", n = 3 * message$n_dev_fields, size = 1)
+        n_dev_fields <- readBin(con = con, what = "int", n = 1, size = 1, signed = FALSE)
+        dev_field_definition <- .processFieldDefs(
+            readBin(con = con, what = "raw", n = 3 * n_dev_fields, size = 1)
         )
+    } else {
+        dev_field_definition = NULL
     }
-    return(list(message = message))
+    
+    message <- new("FitDefinitionMessage",
+        header = message_header,
+        is_little_endian = (architecture == "little"),
+        global_message_number = global_message_num,
+        field_defs = field_definition,
+        dev_field_defs = dev_field_definition
+    )
+    
+    return(message)
 }
 
 
@@ -94,5 +104,69 @@
                          names = definition$field_definition$field_def_num, class = "data.frame")
     #colnames(message) <- definition$field_definition$field_def_num
     return(message)
+}
+
+.readMessage_data <- function(con, header, definition) {
+    
+    fieldTypes <- definition@field_defs$base_type
+    sizes <- definition@field_defs$size
+    
+    message <- vector(mode = "list", length = length(fieldTypes))
+    for(i in seq_along(fieldTypes)) {
+        
+        if( fieldTypes[i] %in% names(data_type_lookup) ) {
+            
+            readInfo <- data_type_lookup[[ fieldTypes[i] ]]
+            
+            ## a single field can have an array of values 
+            single_size <- prod(as.integer(readInfo[3:4]))
+            
+            n_values <- sizes[i] %/% single_size
+            if(fieldTypes[i] == "07") {
+                raw_bytes <- readBin(con = con, what = "raw", n = n_values, size = 1)
+                message[[i]] <- rawToChar(raw_bytes)
+            } else {
+                for(j in seq_len( n_values ) ) {
+                    
+                    dat <- readBin(con, what = readInfo[[1]], signed = readInfo[[2]],
+                                   size = readInfo[[3]], n = readInfo[[4]], 
+                                   endian = ifelse(definition@is_little_endian, "little", "big"))
+                    
+                    ## if we have unsigned ints, turn the bits into a numeric
+                    if(fieldTypes[i] %in% c('86', '8c')) {
+                        if(definition@is_little_endian) {
+                            bits <- as.logical(rawToBits(dat[1:4]))
+                        } else {
+                            bits <- as.logical(rawToBits(dat[4:1]))
+                        }
+                        dat <- sum(2^(.subset(0:31, bits)))
+                    }
+                    
+                    if(n_values == 1) {
+                        message[[i]] <- c(message[[i]], dat)
+                    } else { ## put multiple values in a list, otherwise the tibble has columns with different lengths.
+                        if(j == 1) {
+                            message[[i]] <- list(dat)
+                        } else {
+                            message[[i]][[1]] <- c(message[[i]][[1]], dat)
+                        }
+                    }
+                }
+            }
+        } else {
+            ## we end up here if the datatype is not defined in the FIT spec
+            #message("unknown data type")
+            readBin(con, what = "integer", size = 1, n = sizes[i])
+            message[[i]] <- 0
+        }
+    }
+    
+    names(message) <- definition@field_defs$field_def_num
+    
+    new("FitDataMessage",
+        header = header,
+        definition = definition,
+        fields = message
+    )
 }
 
